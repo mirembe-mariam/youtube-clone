7ffd5e868118ee7134861f0574157c080410af35
mirembe-mariam <mirembemariam3@gmail.com>
2023-03-25 16:29:31 +0300

    we were targeting the search input filed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import "./App.css";
 import Header from "./Header/Header.js";
-import Sidebar from "./Sidebar/Sidebar.js"
+import Sidebar from "./Sidebar/Sidebar.js";
 import Recommendedvideos from "./Recommendedvideos/Recommendedvideos";
-
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
 function App() {
   return (
     <div className="app">
-    <Header />
-    <div className="app__body"> {/*we create this for the body of the clone and it will consisit of the remaining two components*/}
-    <Sidebar />
-    <Recommendedvideos />
-     </div>
+      <Router>
+        {/*is used to figure out which component to show eg on the home which component shld be shown
+      should we show trending or subscriptions*/}
+      <Header/>{/*we want the header to be shown on the '3000 port' not only wen i type '3000/search'*/}
+        <Routes>
+          {/*<Route path="/" element={<h1>Home page</h1>} />*/}
+          <Route path="/search" element={<h1>Search Page </h1>} />
+          {/*<Route path="/search" element={<div><Header /><h1>Search Page</h1></div>}/>*/}
+        
+          <Route path="/" element={<div className="app__body"><Sidebar /><Recommendedvideos /></div>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+// In React Router v6, all routes should be wrapped inside the Routes component, and you can use the element prop to render a component.
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import MenuSharpIcon from "@material-ui/icons/MenuSharp";
 import SearchIcon from "@material-ui/icons/Search";
@@ -7,26 +7,43 @@ import MicIcon from "@material-ui/icons/Mic";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import AppsIcon from "@material-ui/icons/Apps";
 import Avatar from "@material-ui/core/Avatar";
+import { NavLink } from "react-router-dom";
+
 function Header() {
+  const [inputSearch, setInputSearch] = useState("");
   return (
     <div className="header">
       <div className="header__left">
         <MenuSharpIcon />
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0vYBs7_HrbRjopuvwH2PZ_5dRAz3USmLspA&usqp=CAU"
-          alt=""
-        />
+        <NavLink to="/">
+          <div className="header__logo">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0vYBs7_HrbRjopuvwH2PZ_5dRAz3USmLspA&usqp=CAU"
+              alt=""
+            />
+          </div>
+        </NavLink>
       </div>
+
       <div className="header__input">
-        <input placeholder = "Search" type="text" />
-        <SearchIcon 
-        className="header__inputButton" />
+        <input
+          value={inputSearch}
+          onChange={(e) => setInputSearch(e.target.value)}
+          placeholder="Search"
+          type="text"
+        />
+        <NavLink to={`/search/${inputSearch}`}>
+          {" "}
+          {/*link prevents refreshing unlike '<a></a>'. so here we want when we click the search icon
+        it takes us to the search page* and also ${inputSearch} this means let it take me to what i ahve put in my search input*/}
+          <SearchIcon className="header__inputButton" />
+        </NavLink>
         <MicIcon className="header__mic" />
       </div>
       <div className="header__icons">
         <VideoCallIcon className="header__icon" />
-        <AppsIcon className="header__icon"/>
-        <NotificationsIcon className="header__icon"/>
+        <AppsIcon className="header__icon" />
+        <NotificationsIcon className="header__icon" />
         <Avatar
           alt="mirembe mariam"
           src="https://64.media.tumblr.com/85e246a7c69ba713d6be1bacd1db3aac/tumblr_p2sz8pkeJt1tlflqio1_1280.jpg"
